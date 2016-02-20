98607ac2347dbf2687ca39a1e16e0773eda27a4a
Cyrill Schumacher <cyrill@schumacher.fm>
2016-02-20 12:22:28 +0100

    maillog: refactor to match WriteTo() API of gomail.message

--- maillog/maillog.go
+++ maillog/maillog.go
@@ -2,70 +2,71 @@ package maillog
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path"
+	"strings"
 	"time"
 )
-
-// New creates a new logger by a given directory. If the directory does not exists
-// it will be created recursively. Empty directory means a valid nil logger.
-func New(directory string) (*Logger, error) {
-
-	if directory == "" {
-		return nil, nil
-	}
-
-	if false == isDir(directory) {
-		if err := os.MkdirAll(directory, 0700); err != nil {
-			return nil, fmt.Errorf("Cannot create directory %q because of: %s", directory, err)
-		}
-	}
-
-	logFile := path.Join(directory, fmt.Sprintf("errors_%s.log", time.Now()))
-	f, err := os.OpenFile(logFile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		return nil, err
-	}
-
-	l := &Logger{
-		path:    directory,
-		errlog:  log.New(f, "", log.LstdFlags),
-		ErrFile: logFile,
-	}
-	return l, nil
-}
 
 // Logger logs emails and errors. If nil, nothing gets logged.
 type Logger struct {
 	path    string
 	errlog  *log.Logger
 	ErrFile string
+	hosts   []string
 }
 
-// Write creates a new file with a file name consisting of a time stamp.
-// Writes the data into that file and closes the file afterwards.
-// Each write creates a new file.
-func (l *Logger) Write(p []byte) (n int, err error) {
-	if l == nil {
-		return
+// New creates a new logger by a given directory. If the directory does not exists
+// it will be created recursively. Empty directory means a valid nil logger.
+func New(directory string) *Logger {
+	if directory == "" {
+		return nil
 	}
-	fName := fmt.Sprintf("%s%smail_%s.txt", l.path, string(os.PathSeparator), time.Now())
+	return &Logger{
+		path: directory,
+	}
+}
+
+// Init creates directories and the error log file
+func (l *Logger) Init(hosts ...string) (*Logger, error) {
+	if l == nil {
+		return nil, nil
+	}
+	l.hosts = hosts
+	if false == isDir(l.path) {
+		if err := os.MkdirAll(l.path, 0700); err != nil {
+			return nil, fmt.Errorf("Cannot create directory %q because of: %s", l.path, err)
+		}
+	}
+
+	l.ErrFile = path.Join(l.path, fmt.Sprintf("errors_%s_%d.log", strings.Join(hosts, "_"), time.Now().Unix()))
+
+	f, err := os.OpenFile(l.ErrFile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	l.errlog = log.New(f, "", log.LstdFlags)
+
+	return l, nil
+}
+
+// NewWriter creates a new file with a file name consisting of a time stamp.
+// If it fails to create a file it returns a nilWriteCloser and does not log
+// anymore any data.
+func (l *Logger) NewWriter() io.WriteCloser {
+	if l == nil {
+		return nilWriteCloser{}
+	}
+	fName := fmt.Sprintf("%s%smail_%s_%d.txt", l.path, string(os.PathSeparator), strings.Join(l.hosts, "_"), time.Now().UnixNano())
 	f, err := os.OpenFile(fName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
 	if err != nil {
 		l.Errorf("failed to create %q with error: %s", fName, err)
-		return
+		return nilWriteCloser{}
 	}
-	defer func() {
-		if err := f.Close(); err != nil {
-			l.Errorf("failed to close %q with error: %s", fName, err)
-		}
-	}()
-
-	if n, err = f.Write(p); err != nil {
-		l.Errorf("failed to write to %q with error: %s", fName, err)
-	}
-	return
+	return f
 }
 
 // Errorf writes into the error log file. If the logger is nil
@@ -80,4 +81,16 @@ func (l *Logger) Errorf(format string, v ...interface{}) {
 func isDir(path string) bool {
 	fileInfo, err := os.Stat(path)
 	return fileInfo != nil && fileInfo.IsDir() && err == nil
+}
+
+type nilWriteCloser struct {
+	io.WriteCloser
+}
+
+func (wc nilWriteCloser) Write(p []byte) (n int, err error) {
+	return
+}
+
+func (wc nilWriteCloser) Close() error {
+	return nil
 }
--- maillog/maillog_test.go
+++ maillog/maillog_test.go
@@ -1,7 +1,6 @@
 package maillog_test
 
 import (
-	"io"
 	"io/ioutil"
 	"os"
 	"path"
@@ -12,22 +11,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var _ io.Writer = (*maillog.Logger)(nil)
-
 func TestNewEmpty(t *testing.T) {
 	t.Parallel()
-	l, err := maillog.New("")
+	l, err := maillog.New("").Init()
 	assert.Nil(t, l)
 	assert.Nil(t, err)
 	l.Errorf("hello %d", 4711)
-	_, err = l.Write(nil)
+	wc := l.NewWriter()
+	n, err := wc.Write([]byte("H3ll0"))
 	assert.NoError(t, err)
+	assert.Empty(t, n)
 }
 
 func TestNewFail(t *testing.T) {
 	t.Parallel()
 	testDir := path.Join(string(os.PathSeparator), "testdata") // try to create dir in root
-	l, err := maillog.New(testDir)
+	l, err := maillog.New(testDir).Init()
 	assert.Nil(t, l)
 	assert.EqualError(t, err, "Cannot create directory \"/testdata\" because of: mkdir /testdata: permission denied")
 }
@@ -41,7 +40,7 @@ func TestNewErrorfValid(t *testing.T) {
 			t.Fatal(err)
 		}
 	}()
-	l, err := maillog.New(testDir)
+	l, err := maillog.New(testDir).Init()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -65,13 +64,20 @@ func TestNewWriteValid(t *testing.T) {
 			t.Fatal(err)
 		}
 	}()
-	l, err := maillog.New(testDir)
+	l, err := maillog.New(testDir).Init()
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	wc := l.NewWriter()
+	defer func() {
+		if err := wc.Close(); err != nil {
+			t.Fatal(err)
+		}
+	}()
+
 	var testData = []byte(`Snowden: The @FBI is creating a world where citizens rely on #Apple to defend their rights, rather than the other way around. https://t.co/vdjB6CuB7k`)
-	n, err := l.Write(testData)
+	n, err := wc.Write(testData)
 	if err != nil {
 		t.Fatal(err)
 	}
